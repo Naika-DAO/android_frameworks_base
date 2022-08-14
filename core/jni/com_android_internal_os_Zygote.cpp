9c672490abac007dd4f464ac25eece6f2ed2cd23
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-08-14 15:35:44 +0300

    add a wrapper for execveat(2)
    Needed for exec spawning, to pass custom flags to the kernel.

--- core/jni/com_android_internal_os_Zygote.cpp
+++ core/jni/com_android_internal_os_Zygote.cpp
@@ -94,6 +94,8 @@
 
 #include "nativebridge/native_bridge.h"
 
+#include "ExecStrings.h"
+
 namespace {
 
 // TODO (chriswailes): Add a function to initialize native Zygote data.
@@ -2631,6 +2633,18 @@ static void nativeHandleRuntimeFlagsWrapper(JNIEnv* env, jclass, jint runtime_fl
     HandleRuntimeFlags(env, runtime_flags, nullptr, nullptr);
 }
 
+static jint execveatWrapper(JNIEnv* env, jclass, jint dirFd, jstring javaFilename, jobjectArray javaArgv, jint flags) {
+    ScopedUtfChars path(env, javaFilename);
+    if (path.c_str() == NULL) {
+        return EINVAL;
+    }
+
+    ExecStrings argv(env, javaArgv);
+    TEMP_FAILURE_RETRY(execveat(dirFd, path.c_str(), argv.get(), environ, flags));
+    // execveat never returns on success
+    return errno;
+}
+
 static const JNINativeMethod gMethods[] = {
         {"nativeForkAndSpecialize",
          "(II[II[[IILjava/lang/String;Ljava/lang/String;[I[IZLjava/lang/String;Ljava/lang/"
@@ -2684,6 +2698,7 @@ static const JNINativeMethod gMethods[] = {
         {"nativeAllowFilesOpenedByPreload", "()V",
          (void*)com_android_internal_os_Zygote_nativeAllowFilesOpenedByPreload},
         {"nativeHandleRuntimeFlags", "(I)V", (void*)nativeHandleRuntimeFlagsWrapper},
+        {"execveatWrapper", "(ILjava/lang/String;[Ljava/lang/String;I)I", (void*)execveatWrapper},
 };
 
 int register_com_android_internal_os_Zygote(JNIEnv* env) {
